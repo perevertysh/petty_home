37bea94d68ad32597a0956d757c11d803659d47b
perevertysh <justfcksl@gmail.com>
2019-12-15 20:19:28 +0300

    set up

--- pettyhome/settings.py
+++ pettyhome/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.herokuapp.com']
 
 # Application definition
 
