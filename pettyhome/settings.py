d289c78c31ac5d1aacfdd0156a93e5ff2cf59495
perevertysh <justfcksl@gmail.com>
2019-12-15 20:16:09 +0300

    set up

--- pettyhome/settings.py
+++ pettyhome/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -117,10 +117,6 @@ USE_TZ = True
 STATIC_URL = '/app/pettyhome/static'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, "staticfiles"),
-# ]
 
 MEDIA_URL = '/media/'
 
