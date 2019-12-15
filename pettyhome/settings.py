293f271f6d7c3fbd366c66acc1ea0fbc454e6458
perevertysh <justfcksl@gmail.com>
2019-12-15 20:26:19 +0300

    set up

--- pettyhome/settings.py
+++ pettyhome/settings.py
@@ -13,9 +13,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'pettyhome.herokuapp.com']
 
 # Application definition
 
