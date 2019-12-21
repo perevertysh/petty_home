3e1f395041438de5b12b65e7e2b94d18d159b962
perevertysh <justfcksl@gmail.com>
2019-12-21 19:58:08 +0300

    set up

--- pettyhome/settings.py
+++ pettyhome/settings.py
@@ -27,7 +27,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
-    'whitenoise',
     'petdocs',
     'pets',
     'relocation',
