9c607ead4fa6ae4006e8d946219296f570671834
perevertysh <justfcksl@gmail.com>
2019-12-21 23:47:27 +0300

    fix frontend add search

--- pets/views.py
+++ pets/views.py
@@ -1,19 +1,34 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.urls import reverse_lazy
 from django.views.generic import (ListView,
-                                  CreateView,
-                                  DeleteView,
-                                  UpdateView)
+                                  DetailView)
 from pets.models import Pet
 
 
 class IndexPageView(TemplateView):
+
     template_name = 'index.html'
 
 
 class PetListView(ListView):
+
     model = Pet
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        get_params = self.request.GET.dict()
+
+        # search
+        if get_params.get('q'):
+            qs = qs.filter(name__icontains=get_params.get('q'))
+
+        return qs
+
+
+class PetView(DetailView):
+
+    model = Pet
+
+
 class ContactsView(TemplateView):
+
     template_name = 'contacts.html'
