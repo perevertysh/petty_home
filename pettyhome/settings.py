815f5090efef0aff5ef6250790bd9fcf2dba4f79
perevertysh <justfcksl@gmail.com>
2019-12-21 20:28:52 +0300

    set up

--- pettyhome/settings.py
+++ pettyhome/settings.py
@@ -17,7 +17,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '0.0.0.0', 'pettyhome.herokuapp.com']
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Application definition
 
@@ -115,10 +115,14 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-STATICFILES_DIRS = (
+STATIC_DIR = (
     os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+if DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+else:
+    STATIC_ROOT = os.environ.get('DJANGO_STATIC_ROOT')
+STATICFILES_DIRS = [STATIC_DIR, ]
 STATIC_URL = '/static/'
 
 
